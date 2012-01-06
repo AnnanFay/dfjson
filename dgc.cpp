6ca414e6a6c073327ba93d9e62600be66bca45a7
Annan Fay Yearian <annanfay@gmail.com>
2012-01-06 01:26:47 +0000

    few changes

--- dgc.cpp
+++ dgc.cpp
@@ -8,12 +8,15 @@
 #include <string>
 #include <dfhack/modules/Units.h>
 #include <dfhack/modules/Translation.h>
+#include <dfhack/df/static.inc>
+
 
 #include <boost/lexical_cast.hpp>
 
 //#include <json_spirit.h>
 #include "json_spirit_reader_template.h"
 #include "json_spirit_writer_template.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -48,7 +51,7 @@ DFhackCExport command_result plugin_shutdown ( Core * c )
 
 void   printUnits  (Core * c);
 Array  getDwarves  (Core * c);
-Object getDwarf    (df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials * Materials);
+Object getDwarf    (df_unit * dwarf, DFHack::Translation * Tran, DFHack::VersionInfo * Mem);
 
 DFhackCExport command_result dgc (Core * c, vector <string> & parameters)
 {
@@ -73,8 +76,9 @@ Array getDwarves(Core *c) {
 
     DFHack::Units       * Units      = c->getUnits();
     DFHack::Translation * Tran       = c->getTranslation();
-    DFHack::Materials   * Materials  = c->getMaterials();
-    
+    //DFHack::Materials   * Materials  = c->getMaterials();
+    DFHack::VersionInfo * Mem        = c->vinfo;
+
     // Why? 
     Tran->Start();
     
@@ -82,7 +86,6 @@ Array getDwarves(Core *c) {
     uint32_t dwarven                   = Units->GetDwarfRaceIndex();
     uint32_t civ                       = Units->GetDwarfCivId();
 
-     
     df_unit * unit;
     Array dwarves_vec;
     Object dwarf_obj;
@@ -91,13 +94,13 @@ Array getDwarves(Core *c) {
         unit = unitVec->at(i);
         if (unit->race == dwarven && unit->civ == civ) // && cre->job_counter > 0
         {
-            dwarf_obj = getDwarf(unit, Tran, Materials);
+            dwarf_obj = getDwarf(unit, Tran, Mem);
             dwarves_vec.push_back( dwarf_obj );
         }
     }
 
     Units->Finish();
-    Materials->Finish();
+    //Materials->Finish();
     Tran->Finish();
     
     return dwarves_vec;
@@ -107,10 +110,11 @@ void add(Object dwarf, string name, Value val){
     dwarf.push_back( Pair(name, val) );
 }
 
-Value to_value(df_attrib df_attr){
+Value to_value(df_attrib df_attr, string name = ""){
     Object attr;
 
-    attr.push_back( Pair("unk_0",          Value((long long)df_attr.unk_0)));
+    attr.push_back( Pair("name",           Value(name)));
+    attr.push_back( Pair("rating",         Value((long long)df_attr.unk_0)));
     attr.push_back( Pair("unk_4",          Value((long long)df_attr.unk_4)));
     attr.push_back( Pair("unk_8",          Value((long long)df_attr.unk_8)));
     attr.push_back( Pair("unk_c",          Value((long long)df_attr.unk_c)));
@@ -121,83 +125,74 @@ Value to_value(df_attrib df_attr){
     return Value(attr);
 }
 
+Value to_value(df_skill * df_sk, DFHack::VersionInfo * Mem){
+    Object skill;
+ 
+    skill.push_back( Pair("name",       Value(Mem->getSkill(df_sk->id))));
+    skill.push_back( Pair("id",         Value((long long)df_sk->id)));
+    skill.push_back( Pair("rating",     Value((long long)df_sk->rating)));
+    skill.push_back( Pair("experience", Value((long long)df_sk->experience)));
+    skill.push_back( Pair("unk_c",      Value((long long)df_sk->unk_c)));
+    skill.push_back( Pair("unk_rusty",  Value((long long)df_sk->rusty)));
+    skill.push_back( Pair("unk_14",     Value((long long)df_sk->unk_14)));
+    skill.push_back( Pair("unk_18",     Value((long long)df_sk->unk_18)));
+    skill.push_back( Pair("unk_1c",     Value((long long)df_sk->unk_1c)));
+    
+    return Value(skill);
+}
 
-Value getTraits(df_unit * dwarf) {
+
+Value getTraits(df_unit * dwarf, DFHack::VersionInfo * Mem) {
     uint16_t * dwarf_traits = dwarf->current_soul->traits;
     Object traits;
     
-    std::string trait_names[] = {
-        "ANXIETY",
-        "ANGER",
-        "DEPRESSION",
-        "SELF_CONSCIOUSNESS",
-        "IMMODERATION",
-        "VULNERABILITY",
-        "FRIENDLINESS",
-        "GREGARIOUSNESS",
-        "ASSERTIVENESS",
-        "ACTIVITY_LEVEL",
-        "EXCITEMENT_SEEKING",
-        "CHEERFULNESS",
-        "IMAGINATION",
-        "ARTISTIC_INTEREST",
-        "EMOTIONALITY",
-        "ADVENTUROUSNESS",
-        "INTELLECTUAL_CURIOSITY",
-        "LIBERALISM",
-        "TRUST",
-        "STRAIGHTFORWARDNESS",
-        "ALTRUISM",
-        "COOPERATION",
-        "MODESTY",
-        "SYMPATHY",
-        "SELF_EFFICACY",
-        "ORDERLINESS",
-        "DUTIFULNESS",
-        "ACHIEVEMENT_STRIVING",
-        "SELF_DISCIPLINE",
-        "CAUTIOUSNESS "
-    };
-    
-    /*
-    uint16_t * p_traits = dwarf->current_soul->traits;
-    vector<uint16_t> d_traits;
-    d_traits.assign(p_traits, p_traits + NUM_CREATURE_TRAITS);
-    
-    const Value traits( d_traits.begin(), d_traits.end() );
-    */
-    
     for (int i=0; i < NUM_CREATURE_TRAITS; i++){
-        traits.push_back(Pair(trait_names[i], dwarf_traits[i]));
+        traits.push_back(Pair(Mem->getTraitName((uint32_t)i), dwarf_traits[i]));
     }
            
-    return traits;
+    return Value(traits);
 }
 
-Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials * Materials){
+Value getLabors(df_unit * dwarf, DFHack::VersionInfo * Mem) {
+    uint8_t * dwarf_labors = dwarf->labors;
+    Object labors;
+    
+    for (int i=0; i < NUM_CREATURE_LABORS; i++){
+        try {
+            labors.push_back(Pair(Mem->getLabor((uint32_t)i), dwarf_labors[i]));
+        } catch (...){
+            labors.push_back(Pair("unknown-" + lexical_cast<string>(i), dwarf_labors[i]));
+        }
+    }
+           
+    return Value(labors);
+}
+
+Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::VersionInfo * Mem){
     Object dwarf_obj;
 
     df_soul   * soul     = dwarf->current_soul;
-    uint8_t   * labors   = dwarf->labors;
     df_attrib * physical = dwarf->physical;
 
     df_attrib * mental   = soul->mental;
 
     // Name
     Object name;
-     
-    name.push_back( Pair("First",     Value(dwarf->name.first_name)) );
-    name.push_back( Pair("NickFirst", Value(dwarf->name.nick_name)) );
-    name.push_back( Pair("TranName",  Value(Tran->TranslateName(&dwarf->name, false))) );
+    string given_name = dwarf->name.first_name;
+    given_name[0] = toupper(given_name[0]);
 
-    dwarf_obj.push_back( Pair("Name", Value(name)) );
+    name.push_back( Pair("given-name",   Value(given_name)) );
+    name.push_back( Pair("nickname",    Value(dwarf->name.nick_name)) );
+    name.push_back( Pair("surname",     Value(Tran->TranslateName(&dwarf->name, false))) );
+
+    dwarf_obj.push_back( Pair("name", Value(name)) );
 
     // Profession
-    dwarf_obj.push_back( Pair("Profession", Value(dwarf->profession)) );
-    dwarf_obj.push_back( Pair("CustomProfession", Value(dwarf->custom_profession)) );
+    dwarf_obj.push_back( Pair("profession",         Value(Mem->getProfession(dwarf->profession))) );
+    dwarf_obj.push_back( Pair("customProfession",   Value(dwarf->custom_profession)) );
 
     // Race
-    dwarf_obj.push_back( Pair("Race", Value((long long)dwarf->race)) );
+    dwarf_obj.push_back( Pair("race",               Value((long long)dwarf->race)) );
 
     // Position
     Object pos;
@@ -206,7 +201,7 @@ Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials *
     pos.push_back( Pair("y", Value(dwarf->y)) );
     pos.push_back( Pair("z", Value(dwarf->z)) );
 
-    dwarf_obj.push_back( Pair("Position", Value(pos)) );
+    dwarf_obj.push_back( Pair("position", Value(pos)) );
 
     // Destination
     Object dest;
@@ -215,26 +210,29 @@ Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials *
     dest.push_back( Pair("y", Value(dwarf->dest_y)) );
     dest.push_back( Pair("z", Value(dwarf->dest_z)) );
 
-    dwarf_obj.push_back( Pair("Destination", Value(dest)) );
+    dwarf_obj.push_back( Pair("destination", Value(dest)) );
 
     // Caste
-    dwarf_obj.push_back( Pair("Caste",          Value((long long)dwarf->caste)) );
+    dwarf_obj.push_back( Pair("caste",          Value((long long)dwarf->caste)) );
 
     // Sex
-    dwarf_obj.push_back( Pair("Sex",            Value((long long)dwarf->sex)) );
+    dwarf_obj.push_back( Pair("sex",            Value((long long)dwarf->sex)) );
 
     // ID
-    dwarf_obj.push_back( Pair("ID",             Value((long long)dwarf->id)) );
+    dwarf_obj.push_back( Pair("id",             Value((long long)dwarf->id)) );
 
     // Civ
-    dwarf_obj.push_back( Pair("Civilization",   Value((long long)dwarf->civ)) );
+    dwarf_obj.push_back( Pair("civilization",   Value((long long)dwarf->civ)) );
 
     //Mood
-    dwarf_obj.push_back( Pair("mood",           Value((long long)dwarf->mood)));
-    
-    dwarf_obj.push_back( Pair("pregnancy_timer",Value((long long)dwarf->pregnancy_timer)));
-    dwarf_obj.push_back( Pair("birth_year",     Value((long long)dwarf->birth_year)));
-    dwarf_obj.push_back( Pair("birth_time",     Value((long long)dwarf->birth_time)));
+    try { 
+    dwarf_obj.push_back( Pair("mood",           Value(Mem->getMood(dwarf->mood))));
+    } catch (...){}
+   
+    //Birth Stuff
+    dwarf_obj.push_back( Pair("pregnancy-timer",Value((long long)dwarf->pregnancy_timer)));
+    dwarf_obj.push_back( Pair("birth-year",     Value((long long)dwarf->birth_year)));
+    dwarf_obj.push_back( Pair("birth-time",     Value((long long)dwarf->birth_time)));
     
     
     //df_unit * unk_234; // suspiciously close to the pregnancy/birth stuff. Mother?
@@ -251,11 +249,22 @@ Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials *
     // Stats - Physical
     // Convert array to vector
     df_attrib * p_phy = dwarf->physical;
-    vector<df_attrib> d_phy;
-    d_phy.assign(p_phy, p_phy + NUM_CREATURE_PHYSICAL_ATTRIBUTES);
-    
-    for (int i=0; i < d_phy.size(); i++){
-        physical_attrs.push_back( to_value(d_phy[i]) );
+
+    string physical_attr_names[NUM_CREATURE_PHYSICAL_ATTRIBUTES] = {
+        "strength",
+        "agility",
+        "toughness",
+        "endurance",
+        "recuperation",
+        "disease-resistance"
+    };
+
+
+
+
+
+    for (int i=0; i < NUM_CREATURE_PHYSICAL_ATTRIBUTES; i++){
+        physical_attrs.push_back( to_value(p_phy[i], physical_attr_names[i]) );
     } 
 
     stats.push_back( Pair("physical",   Value(physical_attrs)) );
@@ -278,18 +287,7 @@ Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials *
     vector<df_skill*> dskills = dwarf->current_soul->skills;
     
     for (int i=0; i < dskills.size(); i++){
-        Object skill;
-
-        skill.push_back( Pair("id",         Value((long long)dskills[i]->id)));
-        skill.push_back( Pair("rating",     Value((long long)dskills[i]->rating)));
-        skill.push_back( Pair("experience", Value((long long)dskills[i]->experience)));
-        skill.push_back( Pair("unk_c",      Value((long long)dskills[i]->unk_c)));
-        skill.push_back( Pair("unk_rusty",  Value((long long)dskills[i]->rusty)));
-        skill.push_back( Pair("unk_14",     Value((long long)dskills[i]->unk_14)));
-        skill.push_back( Pair("unk_18",     Value((long long)dskills[i]->unk_18)));
-        skill.push_back( Pair("unk_1c",     Value((long long)dskills[i]->unk_1c)));
-        
-        skills.push_back( Value(skill) );
+        skills.push_back( to_value(dskills[i], Mem) );
     }    
     
     stats.push_back( Pair("skills",     Value(skills)) );
@@ -314,111 +312,27 @@ Object getDwarf(df_unit * dwarf, DFHack::Translation * Tran, DFHack::Materials *
     stats.push_back( Pair("likes",     Value(likes)) );
     
     // Stats - Traits
-
-    const Value traits = getTraits(dwarf);
+    const Value traits = getTraits(dwarf, Mem);
     stats.push_back( Pair("traits",     traits) );
 
     // Stats - Add dwarf object
     dwarf_obj.push_back( Pair("stats",  Value(stats)) );
  
-
     //df_name artifact_name; // 5e0
     
     // Labors
-    uint8_t * p_labors = dwarf->labors;
-    vector<uint8_t> d_labors;
-    d_labors.assign(p_labors, p_labors + NUM_CREATURE_LABORS);
-     
-    const Value v_labors( d_labors.begin(), d_labors.end() );
-
-    dwarf_obj.push_back( Pair("labors",     v_labors) );
-
+    const Value labors = getLabors(dwarf, Mem);
+    dwarf_obj.push_back( Pair("labors",     labors) );
+    
     //std::vector<df_thought *> thoughts;
     
     dwarf_obj.push_back( Pair("happiness",          Value((long long)dwarf->happiness)));
-    dwarf_obj.push_back( Pair("hist_figure_id",     Value((long long)dwarf->hist_figure_id)));
-    dwarf_obj.push_back( Pair("able_stand",         Value((long long)dwarf->able_stand)));
-    dwarf_obj.push_back( Pair("able_stand_impair",  Value((long long)dwarf->able_stand_impair)));
-    dwarf_obj.push_back( Pair("able_grasp",         Value((long long)dwarf->able_grasp)));
-    dwarf_obj.push_back( Pair("able_grasp_impair",  Value((long long)dwarf->able_grasp_impair)));
+    dwarf_obj.push_back( Pair("hist-figure-id",     Value((long long)dwarf->hist_figure_id)));
+    dwarf_obj.push_back( Pair("able-stand",         Value((long long)dwarf->able_stand)));
+    dwarf_obj.push_back( Pair("able-stand-impair",  Value((long long)dwarf->able_stand_impair)));
+    dwarf_obj.push_back( Pair("able-grasp",         Value((long long)dwarf->able_grasp)));
+    dwarf_obj.push_back( Pair("able-grasp-impair",  Value((long long)dwarf->able_grasp_impair)));
 
     return dwarf_obj;
-}
-void printUnits(Core *c)
-{
-    DFHack::Units       *Units     = c->getUnits();
-    DFHack::Translation *Tran      = c->getTranslation();
-    DFHack::Materials   *materials = c->getMaterials();
-    
-    Tran->Start();
-    
-    static vector <df_unit*> *unitVec = Units->creatures;
-    uint32_t race                     = Units->GetDwarfRaceIndex();
-    uint32_t civ                      = Units->GetDwarfCivId();
-
-    df_unit *cre;
-    for (unsigned i=0; i < unitVec->size(); ++i)
-    {
-        cre = unitVec->at(i);
-        if (cre->race == race && cre->civ == civ && cre->job_counter > 0)
-        {
-            df_soul * soul = cre->current_soul;
-            uint8_t * labors = cre->labors;
-            df_attrib * physical = cre->physical;
-
-            df_attrib * mental = soul->mental;
-            
-            c->con << cre;
-            c->con << " {" << std::endl << "    ";
-            
-            c->con << "[" << cre->name.first_name << " " << Tran->TranslateName(&cre->name, false) << "]" << std::endl << "    ";
-            
-            c->con << "skills: ";
-            for (int i=0; i < soul->skills.size(); i++){
-                c->con << soul->skills[i]->id << " | ";
-            }
-            c->con << std::endl << "    ";
-            
-            c->con << "likes: ";
-            for (int i=0; i < soul->likes.size(); i++){
-                c->con << soul->likes[i]->type << " | ";
-            }
-            c->con << std::endl << "    ";
-
-            c->con << "traits: ";
-            c->con << soul->traits << " | ";
-            c->con << std::endl << "    ";
-            
-            c->con << "labors: ";
-            c->con << labors << " | ";
-            c->con << std::endl << "    ";
- 
-            c->con << "mental: ";
-            c->con << mental->unk_0 << " | ";
-            c->con << mental->unk_4 << " | ";
-            c->con << mental->unk_8 << " | ";
-            c->con << mental->unk_c << " | ";
-            c->con << mental->unk_10 << " | ";
-            c->con << mental->unk_14 << " | ";
-            c->con << mental->unk_18 << " | ";
-            c->con << std::endl << "    ";
-            
-            c->con << "physical: ";
-            c->con << physical->unk_0 << " | ";
-            c->con << physical->unk_4 << " | ";
-            c->con << physical->unk_8 << " | ";
-            c->con << physical->unk_c << " | ";
-            c->con << physical->unk_10 << " | ";
-            c->con << physical->unk_14 << " | ";
-            c->con << physical->unk_18 << " | ";
-            c->con << std::endl << "    ";
-            
-            c->con << "happiness: ";
-            c->con << cre->happiness;
-            c->con << "}";
-            c->con << std::endl;
-
-        }
-    }
 }
 
